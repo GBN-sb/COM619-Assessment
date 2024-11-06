db034a9217d0b6350a4676e26096e85f831ff8d7
GBN-sb <registrations@sam.brookesonline.com>
2024-11-06 16:07:31 +0000

    test new code

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,6 +1,6 @@
 # tests/test_main.py
 
-from app.main import add, subtract
+from app.main import add, subtract, multiply
 
 
 def test_add():
@@ -15,3 +15,10 @@ def test_subtract():
     assert subtract(0, 0) == 0
     assert subtract(-1, 1) == -2
     assert subtract(-1, -1) == 0
+
+
+def test_multiply():
+    assert multiply(10, 5) == 50
+    assert multiply(0, 0) == 0
+    assert multiply(-1, 1) == -1
+    assert multiply(-1, -1) == 1
